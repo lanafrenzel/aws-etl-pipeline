e2359306e628056fe21ddd317c326ed869de48fd
Lana Frenzel <166107394+lanafrenzel@users.noreply.github.com>
2025-06-18 13:09:29 +0200

    Update extract_api_data.py

--- lambda/extract_api_data.py
+++ lambda/extract_api_data.py
@@ -2,7 +2,7 @@ import json
 import os
 import boto3
 import urllib3
-from datetime import datetime
+from  import datetime
 from botocore.exceptions import ClientError
 
 http = urllib3.PoolManager()
